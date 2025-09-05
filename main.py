fdbe5e43089b3ed20423b0a6c7e3120a0aa22ed1
RaihanKabir277 <raihank0198@gmail.com>
2025-09-05 19:22:31 +0600

    smtplib imported for send message my self when iss is overhead

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 from datetime import datetime
+import smtplib
 
 MY_LAT = 51.507351 # Your latitude
 MY_LONG = -0.127758 # Your longitude
+MY_EMAIL = "raihankabir123@gmail.com" #demo email
+MY_PASS = "12345678abc"  #demopass
 
 def is_iss_overhead():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -29,10 +32,19 @@ def is_night():
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-    time_now = datetime.now()
+    time_now = datetime.now().hour
     if time_now >= sunset or time_now <= sunrise:
-        print("its dark")
+        # print("its dark")
+        return True
 
-
+if is_iss_overhead() and is_night():
+    connection = smtplib.SMTP("smtp.gmail.com")
+    connection.starttls()
+    connection.login(MY_EMAIL, MY_PASS)
+    connection.sendmail(
+        from_addr=MY_EMAIL,
+        to_addrs=MY_EMAIL,
+        msg="Subject:LOOK UP \n\n The iss is above you in the sky."
+    )
 
 
